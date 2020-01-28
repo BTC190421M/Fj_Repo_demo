ef868ddcc4cc4d8e8915fe019f1ebd1fa9cf7ac1
ahfayej08 <ahfayej08@gmail.com>
2020-01-28 18:38:18 -0500

    first Commit

--- Demo_MVN_Project/src/test/java/com/bit/test/BaseTest.java
+++ Demo_MVN_Project/src/test/java/com/bit/test/BaseTest.java
@@ -1,6 +1,7 @@
 package com.bit.test;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
 	WebDriver dr;
-	
+
 	@Before
 	public void openBrowser() {
 		System.setProperty("webdriver.chrome.driver", "/Users/fayejahmed/Downloads/chromedriver");
@@ -17,17 +18,38 @@ public class BaseTest {
 		dr.get("https://www.google.com/");
 
 	}
-	
+
 	@Test
 	public void test() {
 		String actualUrl = dr.getCurrentUrl();
-		System.out.println("Actual Url "+actualUrl);
-		System.out.println("Title of the page "+dr.getTitle());
+		System.out.println("Actual Url " + actualUrl);
+		System.out.println("Title of the page " + dr.getTitle());
+		if (actualUrl.equals("helo")) {
+			Assert.assertTrue(true);
+			System.out.println("URL matched");
+		} else {
+			Assert.assertTrue(false);
+			System.out.println("URL did not matched");
+		}
+
+	}
+	@Test
+	public void test1() {
+		String actualTitle = dr.getTitle();
 		
+		System.out.println("Actual title " + actualTitle);
+		System.out.println("Title of the page " + "|"+ dr.getTitle() + "|");
 		
+		if (actualTitle.equals("Google")) {
+			Assert.assertTrue(true);
+			System.out.println("title matched");
+
+		} else {
+			Assert.assertTrue(false);
+			System.out.println("title didn't match");
+		}
 		
 	}
-	
 
 	@After
 	public void closeBrowser() throws InterruptedException {
@@ -35,13 +57,5 @@ public class BaseTest {
 		Thread.sleep(3000);
 		dr.quit();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
